6569d7d4a7b1fb33adb376fc554cc2bd010d0b11
Vitalii Maliuk <malyuk.vitalick@gmail.com>
2022-08-11 19:43:44 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = new Map(bracketsConfig);
+  let result = [];
+  str = str.split('');
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== brackets.get(result[result.length - 1])) {
+      result.push(str[i]);
+    } else {
+      result.pop();
+    }
+  }
+  if (result.length === 0) {
+    return true;
+  } else {
+    return false;
+  }
 }
